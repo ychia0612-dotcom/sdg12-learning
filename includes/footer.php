a5f4c21a3dc535257f92e24a2ccb5094e0b81f8f
ychia0612-dotcom <ychia0612@gmail.com>
2026-05-14 10:03:50 +0800

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,9 @@
 <?php include_once 'greendefender.php'; ?>
+<?php include_once 'db.php'; ?>
 
 <!-- 網站頁腳 -->
 <div style="text-align:center; margin:10px 0;">
-    🌐 網站總訪問人次：開發中
+    🌐 網站總訪問人次：<?php echo get_total_visits(); ?>
 </div>
 <div style="text-align:center; margin:10px 0;">
     © 2026 SDG12 永續生活家學習平台
